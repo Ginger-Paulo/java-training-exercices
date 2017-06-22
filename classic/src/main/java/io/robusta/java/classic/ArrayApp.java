83dbfc4ef4d3f75a5a2c68343d4db47e3469f69b
Ginger-Paulo <totti_84@hotmail.fr>
2017-06-22 08:46:08 +0200

    wip classic

--- classic/src/main/java/io/robusta/java/classic/ArrayApp.java
+++ classic/src/main/java/io/robusta/java/classic/ArrayApp.java
@@ -1,22 +1,46 @@
 package io.robusta.java.classic;
 
+import java.util.Arrays;
+
 /**
  * Created by nicorama on 21/06/2017.
  */
 public class ArrayApp {
 
-    int[] buildArray(int a, int b, int c){
-        return null;
+    @SuppressWarnings("null")
+	int[] buildArray(int a, int b, int c){
+    	
+    	int tabTest [] = {a,b,c};
+        return tabTest;
     }
 
 
     boolean equality(int[] array1, int[] array2){
 
-        return false;
+    	
+    	if(array1.length!=array2.length) 
+    		   return false;
+
+    		for(int index=0;index<array1.length;index++)
+    		{
+    		    if (array1[index]!=array2[index])
+    		         return false;
+
+    		}
+    		return true;
     }
 
     String asString (int [] numbers){
-        return null;
+    	
+    	String result =  "";
+    	
+    	for (int i = 0; i< numbers.length;i++){
+    		
+    		result  = result + numbers [i] +":";
+    	}
+    	
+    	
+        return result;
     }
 
     String asStringJoin (int [] numbers){
